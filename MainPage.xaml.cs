26b6c16a7b51a5f42b5068904306b5989ec242b4
ARTUR <arturdiniz06@gmail.com>
2023-09-01 22:48:11 -0300

    Dice Roller 2

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace DiceRoller;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+
+	void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+	{
+
+		Label numLados = new Label();
+		//  numLados.SetBinding(Label.TextProperty, new Binding("SelectedItem", source: picker));
+
+		var qtLados = new List<string>();
+		qtLados.Add("4");
+		qtLados.Add("6");
+		qtLados.Add("10");
+		qtLados.Add("20");
+		qtLados.Add("100");
+
+
+	}
+
+            
+}
+
+ 
+
