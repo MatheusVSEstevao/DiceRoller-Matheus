da78f3dffebcd4ab11a904ca82692c3945286a1d
Artur Diniz <102334516+Artur-Diniz@users.noreply.github.com>
2023-09-08 04:48:32 -0300

    Last Commit

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 
 
 namespace DiceRoller;
@@ -6,23 +7,48 @@ public partial class MainPage : ContentPage
 {
     private Random rnd = new Random();
 
+
+
+
     public MainPage()
     {
         InitializeComponent();
+
+        Lados.SelectedIndexChanged += Picker_SelectedIndexChanged;
+
+    }
+
+    public void Picker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      
+        string valorSelecionado = (string)Lados.SelectedItem;
+
+       
+      
     }
 
 
     private void GerarNumeroAleatorio_Click(object sender, EventArgs e)
     {
-        
-        int numeroAleatorio = rnd.Next(1, 10);
+        string valorSelecionado = (string)Lados.SelectedItem;
 
+        if (int.TryParse(valorSelecionado, out int maiorPossibilidade))
+        {
 
-        NumeroAleatorioLabel.Text = numeroAleatorio.ToString();
+            int numeroAleatorio = rnd.Next(1, maiorPossibilidade + 1);
+
+            NumeroAleatorioLabel.Text = numeroAleatorio.ToString();
+        }
+        else
+        {
+           
+            NumeroAleatorioLabel.Text = "Selecione a quantidade de lados.";
+        }
     }
 
 
 
-   
+
+
 }
 
